b88c5b7ec620d561b49ab8ffec7160fd1634d9b2
GustavoSilvera <gustavo.r.silvera@gmail.com>
2021-02-01 16:28:39 -0800

    first commit

---
+++ Source/SRanipalEyeTest/SRanipalEyeTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SRanipalEyeTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SRANIPALEYETEST_API ASRanipalEyeTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
