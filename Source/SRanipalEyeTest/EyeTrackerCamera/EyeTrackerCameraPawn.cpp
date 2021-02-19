cad8b3ab0744aacb463a011ab97bc26ac9de6538
GustavoSilvera <gustavo.r.silvera@gmail.com>
2021-02-18 17:19:17 -0800

    fixed formatting

--- Source/SRanipalEyeTest/EyeTrackerCamera/EyeTrackerCameraPawn.cpp
+++ Source/SRanipalEyeTest/EyeTrackerCamera/EyeTrackerCameraPawn.cpp
@@ -1,19 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EyeTrackerCameraPawn.h"
-#include "Kismet/KismetSystemLibrary.h"		   // PrintString, QuitGame
+#include "Kismet/KismetSystemLibrary.h" // PrintString, QuitGame
 #include "HeadMountedDisplayFunctionLibrary.h"
-#include "Kismet/GameplayStatics.h"			   // GetPlayerController
-#include "DrawDebugHelpers.h"				   // Debug Line/Sphere
+#include "Kismet/GameplayStatics.h" // GetPlayerController
+#include "DrawDebugHelpers.h"		// Debug Line/Sphere
 
 #include <iostream> // IO
-#include <fstream> // File IO
+#include <fstream>	// File IO
 
 // Sets default values
 AEyeTrackerCameraPawn::AEyeTrackerCameraPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	// Set this pawn to be controlled by first (only) player
@@ -21,14 +20,13 @@ AEyeTrackerCameraPawn::AEyeTrackerCameraPawn()
 
 	// Spawn the RootComponent and Camera for the VR camera
 	VRCameraRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRCameraRoot"));
-	VRCameraRoot->SetupAttachment(GetRootComponent());		// The vehicle blueprint itself
+	VRCameraRoot->SetupAttachment(GetRootComponent()); // The vehicle blueprint itself
 
 	// Create a camera and attach to root component
 	FirstPersonCam = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCam"));
 	FirstPersonCam->SetupAttachment(VRCameraRoot);
 	FirstPersonCam->bUsePawnControlRotation = false; // free for VR movement
 	FirstPersonCam->FieldOfView = 90.0f;			 // editable
-
 }
 void AEyeTrackerCameraPawn::ErrMsg(const FString &message, const bool isFatal = false)
 {
@@ -45,32 +43,33 @@ void AEyeTrackerCameraPawn::BeginPlay()
 	Super::BeginPlay();
 
 	// get the world
-    World = GetWorld();
+	World = GetWorld();
 
 	pc = UGameplayStatics::GetPlayerController(World, 0); // main player (0) controller
 	// Now we'll begin with setting up the VR Origin logic
 	UHeadMountedDisplayFunctionLibrary::SetTrackingOrigin(EHMDTrackingOrigin::Eye); // Also have Floor & Stage Level
-	
-    // Initialize the SRanipal eye tracker (WINDOWS ONLY)
+
+	// Initialize the SRanipal eye tracker (WINDOWS ONLY)
 	SRanipalFramework = SRanipalEye_Framework::Instance();
-    SRanipal = SRanipalEye_Core::Instance();
+	SRanipal = SRanipalEye_Core::Instance();
 	check(SRanipal != nullptr);
 	SRanipalFramework->StartFramework(SupportedEyeVersion::version1); // same result with version 2
 
 	SRanipal->GetEyeData_(&EyeData);
-    TimestampRef = EyeData.timestamp;
+	TimestampRef = EyeData.timestamp;
 }
 
 void AEyeTrackerCameraPawn::BeginDestroy()
 {
 	WriteDataToFile();
-    if (SRanipal)
-        SRanipalEye_Core::DestroyEyeModule();
-	if (SRanipalFramework){
+	if (SRanipal)
+		SRanipalEye_Core::DestroyEyeModule();
+	if (SRanipalFramework)
+	{
 		SRanipalFramework->StopFramework();
 		SRanipalEye_Framework::DestroyEyeFramework();
 	}
-    Super::BeginDestroy();
+	Super::BeginDestroy();
 }
 
 // Called every frame
@@ -78,52 +77,51 @@ void AEyeTrackerCameraPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-    // Assign Left/Right Gaze direction
+	// Assign Left/Right Gaze direction
 	check(SRanipal != nullptr);
 	// all the SRanipal data will be stored in this structure
 	CustomEyeData ced;
 	// Getting real eye tracker data
 	SRanipal->GetEyeData_(&EyeData);
-    ced.TimestampSR = EyeData.timestamp - TimestampRef;
-	// ftime_s is used to get the UE4 (carla) timestamp of the world at this tick
-    double ftime_s = UGameplayStatics::GetRealTimeSeconds(World);
-    // Assigns EyeOrigin and Gaze direction (normalized) of combined gaze
-    ced.GazeValid = SRanipal->GetGazeRay(GazeIndex::COMBINE, ced.EyeOrigin, ced.GazeRay);
-    // Assign Left/Right Gaze direction
-    /// NOTE: the eye gazes are reversed at the lowest level bc SRanipal has a bug in their
-    // libraries that flips these when collected from the sensor. We can verify this by
-    // plotting debug lines for the left and right eye gazes and notice that if we close
-    // one eye, the OTHER eye's gaze is null, when it should be the closed eye instead
-    ced.LGazeValid = SRanipal->GetGazeRay(GazeIndex::LEFT, ced.LEyeOrigin, ced.RGazeRay);
-    ced.RGazeValid = SRanipal->GetGazeRay(GazeIndex::RIGHT, ced.REyeOrigin, ced.LGazeRay);
-    // Assign Eye openness
-    ced.LEyeOpenValid = SRanipal->GetEyeOpenness(EyeIndex::LEFT, ced.LEyeOpenness);
-    ced.REyeOpenValid = SRanipal->GetEyeOpenness(EyeIndex::RIGHT, ced.REyeOpenness);
-    // Assign Pupil positions
-    ced.LPupilPosValid = SRanipal->GetPupilPosition(EyeIndex::LEFT, ced.LPupilPos);
-    ced.RPupilPosValid = SRanipal->GetPupilPosition(EyeIndex::RIGHT, ced.RPupilPos);
-    // Assign Pupil Diameters
-    ced.LPupilDiameter = EyeData.verbose_data.left.pupil_diameter_mm;
-    ced.RPupilDiameter = EyeData.verbose_data.right.pupil_diameter_mm;
-    // Assign data convergence
-    ced.ConvergenceDist = EyeData.verbose_data.combined.convergence_distance_mm;
-    // Assign FFocus information
-    /// NOTE: requires the player's camera manager
-    /// NOTE: the ECollisionChannel::ECC_Pawn is set to ignore the pawn/vehicle for the line-trace
-    FVector FocusOrigin, FocusDirection; // These are ignored since we compute them in EgoVehicle
-    SRanipal->Focus(GazeIndex::COMBINE, pc->PlayerCameraManager,
-                    ECC_Pawn, FocusInfo,
-                    FocusOrigin, FocusDirection);
-    if(FocusInfo.actor != nullptr){
-        FocusInfo.actor->GetName(ced.FocusActorName);
-    }
-    else{
-        ced.FocusActorName = FString(""); // empty string, not looking at any actor
-    }
-    ced.FocusActorPoint = FocusInfo.point;
-    ced.FocusActorDist = FocusInfo.distance;
+	ced.TimestampSR = EyeData.timestamp - TimestampRef;
+	ced.FrameSequence = EyeData.frame_sequence; // add raw "frame sequence" from SRanipal Eyes Enums
+												// ftime_s is used to get the UE4 (carla) timestamp of the world at this tick
+	double ftime_s = UGameplayStatics::GetRealTimeSeconds(World);
+	// Assigns EyeOrigin and Gaze direction (normalized) of combined gaze
+	ced.GazeValid = SRanipal->GetGazeRay(GazeIndex::COMBINE, ced.EyeOrigin, ced.GazeRay);
+	// Assign Left/Right Gaze direction
+	/// NOTE: the eye gazes are reversed at the lowest level bc SRanipal has a bug in their
+	// libraries that flips these when collected from the sensor. We can verify this by
+	// plotting debug lines for the left and right eye gazes and notice that if we close
+	// one eye, the OTHER eye's gaze is null, when it should be the closed eye instead
+	ced.LGazeValid = SRanipal->GetGazeRay(GazeIndex::LEFT, ced.LEyeOrigin, ced.LGazeRay);
+	ced.RGazeValid = SRanipal->GetGazeRay(GazeIndex::RIGHT, ced.REyeOrigin, ced.RGazeRay);
+	// Assign Eye openness
+	ced.LEyeOpenValid = SRanipal->GetEyeOpenness(EyeIndex::LEFT, ced.LEyeOpenness);
+	ced.REyeOpenValid = SRanipal->GetEyeOpenness(EyeIndex::RIGHT, ced.REyeOpenness);
+	// Assign Pupil positions
+	ced.LPupilPosValid = SRanipal->GetPupilPosition(EyeIndex::LEFT, ced.LPupilPos);
+	ced.RPupilPosValid = SRanipal->GetPupilPosition(EyeIndex::RIGHT, ced.RPupilPos);
+	// Assign Pupil Diameters
+	ced.LPupilDiameter = EyeData.verbose_data.left.pupil_diameter_mm;
+	ced.RPupilDiameter = EyeData.verbose_data.right.pupil_diameter_mm;
+	// Assign data convergence
+	ced.ConvergenceDist = EyeData.verbose_data.combined.convergence_distance_mm;
+	// Assign FFocus information
+	/// NOTE: requires the player's camera manager
+	/// NOTE: the ECollisionChannel::ECC_Pawn is set to ignore the pawn/vehicle for the line-trace
+	FVector FocusOrigin, FocusDirection; // These are ignored since we compute them in EgoVehicle
+	SRanipal->Focus(GazeIndex::COMBINE, pc->PlayerCameraManager,
+					ECC_Pawn, FocusInfo,
+					FocusOrigin, FocusDirection);
+	if (FocusInfo.actor != nullptr)
+		FocusInfo.actor->GetName(ced.FocusActorName);
+	else
+		ced.FocusActorName = FString(""); // empty string, not looking at any actor
+	ced.FocusActorPoint = FocusInfo.point;
+	ced.FocusActorDist = FocusInfo.distance;
 	// Update the UE4 tick timestamp
-    ced.TimestampUE4 = int64_t(ftime_s * 1000);
+	ced.TimestampUE4 = int64_t(ftime_s * 1000);
 	// get information about the VR world
 	const float ScaleToUE4Meters = UHeadMountedDisplayFunctionLibrary::GetWorldToMetersScale(World);
 	FRotator WorldRot = FirstPersonCam->GetComponentRotation(); // based on the hmd rotation
@@ -142,28 +140,35 @@ void AEyeTrackerCameraPawn::Tick(float DeltaTime)
 }
 
 // Called to bind functionality to input
-void AEyeTrackerCameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AEyeTrackerCameraPawn::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
-void WriteToFile(const std::vector<float> &data, const std::string &name){
+template <typename T>
+void WriteToFile(const std::vector<T> &data, const std::string &name)
+{
 	std::ofstream FILE(name + ".txt");
-	for(auto x : data){
+	for (auto x : data)
+	{
 		FILE << x << " ";
 	}
 	FILE.close();
 }
 
-void AEyeTrackerCameraPawn::WriteDataToFile() const {
+void AEyeTrackerCameraPawn::WriteDataToFile() const
+{
 	std::vector<float> TimesSR, TimesUE4;
+	std::vector<int64_t> FrameSeq;
 	// preprocess all data into vector of floats for easy printings
-	for(auto ced : AllData){
+	for (auto ced : AllData)
+	{
 		TimesSR.push_back(ced.TimestampSR / 1000.0f);
 		TimesUE4.push_back(ced.TimestampUE4 / 1000.0f);
+		FrameSeq.push_back(ced.FrameSequence);
 	}
 	// write to file
-	WriteToFile(TimesSR, "sranipal");
-	WriteToFile(TimesUE4, "ue4");
+	WriteToFile(TimesSR, "test_sranipal");
+	WriteToFile(TimesUE4, "test_ue4");
+	WriteToFile(FrameSeq, "test_frameseq");
 }
--- Source/SRanipalEyeTest/EyeTrackerCamera/EyeTrackerCameraPawn.h
+++ Source/SRanipalEyeTest/EyeTrackerCamera/EyeTrackerCameraPawn.h
@@ -4,15 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "Camera/CameraComponent.h"			   // UCameraComponent
-#include "Components/SceneComponent.h"		   // USceneComponent
-#include "Components/InputComponent.h"		   // InputComponent
+#include "Camera/CameraComponent.h"	   // UCameraComponent
+#include "Components/SceneComponent.h" // USceneComponent
+#include "Components/InputComponent.h" // InputComponent
 #include <cstdint>
 #include <vector>
 
 /// WARNING: SRanipal only supports Windows development currently
-// #ifdef _WIN32 
-#include "SRanipalEye.h"      // SRanipal Module Framework
+// #ifdef _WIN32
+#include "SRanipalEye.h"	  // SRanipal Module Framework
 #include "SRanipalEye_Core.h" // SRanipal Eye Tracker
 #include "SRanipalEye_Framework.h"
 
@@ -31,7 +31,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 
 	void WriteDataToFile() const;
 
@@ -54,26 +54,28 @@ private:
 
 	UPROPERTY(Category = Camera, EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent *FirstPersonCam;
-    // Eye Tracker Variables
-    SRanipalEye_Core *SRanipal;               // SRanipalEye_Core.h
-    SRanipalEye_Framework *SRanipalFramework; // SRanipalEye_Framework.h
-    ViveSR::anipal::Eye::EyeData EyeData;     // SRanipal_Eyes_Enums.h
-    FFocusInfo FocusInfo;                     // SRanipal_Eyes_Enums.h
+	// Eye Tracker Variables
+	SRanipalEye_Core *SRanipal;				  // SRanipalEye_Core.h
+	SRanipalEye_Framework *SRanipalFramework; // SRanipalEye_Framework.h
+	ViveSR::anipal::Eye::EyeData EyeData;	  // SRanipal_Eyes_Enums.h
+	FFocusInfo FocusInfo;					  // SRanipal_Eyes_Enums.h
 
-	int64_t TimestampRef = 0;   // reference timestamp (ms) since the hmd started ticking
-	struct CustomEyeData {
-		int64_t TimestampSR = 0;    // timestamp of when the frame was captured by SRanipal. in Seconds
-		int64_t TimestampUE4 = 0; // Timestamp within UE4 of when the EyeTracker Tick() occurred
+	int64_t TimestampRef = 0; // reference timestamp (ms) since the hmd started ticking
+	struct CustomEyeData
+	{
+		int64_t TimestampSR = 0;   // timestamp of when the frame was captured by SRanipal. in Seconds
+		int64_t TimestampUE4 = 0;  // Timestamp within UE4 of when the EyeTracker Tick() occurred
+		int64_t FrameSequence = 0; // Frame seqence given by SRanipal
 		// (Low level) Eye Tracker Data
 		FVector GazeRay{FVector::ZeroVector}; // initialized as {1,0,0}
 		FVector EyeOrigin{-5.60, 0.14, 1.12}; // initialized to {0,0,0} at the camera
 		bool GazeValid = false;
 		float Vergence = -1.0f; // distance to focus point
 		// L/R additional eye tracker data from SRanipal
-		FVector LGazeRay{FVector::ZeroVector};    // Left eye Gaze ray only
+		FVector LGazeRay{FVector::ZeroVector};	  // Left eye Gaze ray only
 		FVector LEyeOrigin{-6.308, 3.247, 1.264}; // initialized to {0,0,0} at the camera
 		bool LGazeValid = false;
-		FVector RGazeRay{FVector::ZeroVector};     // Right eye Gaze ray only
+		FVector RGazeRay{FVector::ZeroVector};	   // Right eye Gaze ray only
 		FVector REyeOrigin{-5.284, -3.269, 1.014}; // initialized to {0,0,0} at the camera
 		bool RGazeValid = false;
 		// Eye openness
@@ -93,11 +95,11 @@ private:
 		// Combined data convergence mm
 		float ConvergenceDist; // convergence distance (in mm)
 		// FFocusInfo
-		FString FocusActorName;  // Tag of the actor being focused on
+		FString FocusActorName;	 // Tag of the actor being focused on
 		FVector FocusActorPoint; // Hit point of the Focus Actor
-		float FocusActorDist;    // Distance to the Focus Actor
+		float FocusActorDist;	 // Distance to the Focus Actor
 	};
 
 	std::vector<CustomEyeData> AllData;
-    // non-SRanipal data fields
+	// non-SRanipal data fields
 };
